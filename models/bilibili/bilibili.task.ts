48f31195b0d667270755ad4a9aa1167c84a96604
snowtafir <fengxuehuayue@gmail.com>
2024-08-03 18:37:46 +0800

    add dynamic check log

--- models/bilibili/bilibili.task.ts
+++ models/bilibili/bilibili.task.ts
@@ -113,8 +113,12 @@ export class BiliTask {
 
       for (let dynamicItem of tempDynamicList) {
         let author = dynamicItem?.modules?.module_author || {};
+        logger.info(`正在检测B站动态 [ ${author?.name} : ${author?.mid} ]`);
         if (!author?.pub_ts) continue; // 如果动态没有发布时间，跳过当前循环
-        if (Number(now - author.pub_ts) > interval) continue; // 如果超过推送时间间隔，跳过当前循环
+        if (Number(now - author.pub_ts) > interval) {
+          logger.info(`超过间隔时间，跳过该up [ ${author?.name} : ${author?.mid} ] ${author?.pub_time} 的动态`);
+          continue;
+        } // 如果超过推送时间间隔，跳过当前循环
         if (dynamicItem.type === "DYNAMIC_TYPE_FORWARD" && !biliConfigData.pushTransmit) continue; // 如果关闭了转发动态的推送，跳过当前循环
         willPushDynamicList.push(dynamicItem);
       }
