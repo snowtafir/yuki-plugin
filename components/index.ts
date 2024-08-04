b5bc07ee3be4e73121d92e8f6864d87f321885cd
snowtafir <fengxuehuayue@gmail.com>
2024-08-04 11:34:48 +0800

    init compile v4

--- components/index.ts
+++
@@ -1,6 +0,0 @@
-const MainPage = (await import('./dynamic/MainPage.js')).default;
-const Help = (await import('./help/Help.js')).default;
-const LoginQrcodePage = (await import('./loginQrcode/Page.js')).default;
-const Version = (await import('./version/Version.js')).default;
-
-export { Help, LoginQrcodePage, MainPage, Version };
