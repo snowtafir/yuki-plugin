9f01f39a88ba15faa2be80428234112faac187cb
snowtafir <fengxuehuayue@gmail.com>
2024-08-12 19:46:48 +0800

    Correct the typos

--- models/bilibili/bilibili.models.ts
+++ models/bilibili/bilibili.models.ts
@@ -78,7 +78,7 @@ export async function pollLoginQRCode(e: any, qrcodeKey: string) {
       // 未扫码
       // 继续轮询
       await new Promise((resolve) => setTimeout(resolve, 2000));
-      (logger ?? Bot.logger)?.mark(`yuki-yuki插件：扫码B站登录：未扫码，轮询中...`);
+      (logger ?? Bot.logger)?.mark(`优纪插件：扫码B站登录：未扫码，轮询中...`);
       return pollLoginQRCode(e, qrcodeKey);
     } else if (data.data.code === 86090) {
       // 已扫码未确认
@@ -108,7 +108,7 @@ export async function checkBiliLogin(e: any) {
     redirect: "follow",
   });
   const resData: any = await res.json()
-  Bot.logger?.mark(`B站动态请求code:${JSON.stringify(resData)}`);
+  Bot.logger?.debug(`B站验证登录状态:${JSON.stringify(resData)}`);
 
   if (resData.code === 0) {
     let uname = resData.data?.uname;
