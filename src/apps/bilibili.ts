f2cec40415ed6cd9db01d8913063a24992c9afdc
snowtafir <fengxuehuayue@gmail.com>
2024-08-18 20:43:46 +0800

    fix some user's latent build-in method err

--- src/apps/bilibili.ts
+++ src/apps/bilibili.ts
@@ -111,7 +111,14 @@ message.use(
         return;
       }
 
-      let name = items.length > 0 ? (items[0].modules.module_author?.name || uid) : uid;
+      let name: string | number
+      if (Array.isArray(items)) {
+        if (items.length > 0) {
+          name = items[0].modules?.module_author?.name || uid
+        }
+      } else {
+        name = uid;
+      }
 
       // 添加新的推送数据
       subData[chatType][chatId].push({
