4b2e0d55b3de54c52617f1fbaa234005ab1c5b02
snowtafir <fengxuehuayue@gmail.com>
2024-10-23 13:17:18 +0800

    optimize: add dm_img params

---
+++ src/models/bilibili/bilibili.dm.img.ts
@@ -0,0 +1,13 @@
+/**获取dm参数 */
+export async function getDmImg() {
+  const dm_img_list = [];
+  //Buffer.from("WebGL 1", 'utf-8').toString("base64") //webgl version的值 WebGL 1 的base64 编码
+  const dm_img_str = 'V2ViR0wgMS';
+  //webgl unmasked renderer的值拼接webgl unmasked vendor的值的base64编码
+  const dm_cover_img_str = 'QU5HTEUgKEludGVsLCBJbnRlbChSKSBIRCBHcmFwaGljcyBEaXJlY3QzRDExIHZzXzVfMCBwc181XzApLCBvciBzaW1pbGFyR29vZ2xlIEluYy4gKEludGVsKQ';
+  return {
+    dm_img_list: dm_img_list,
+    dm_img_str: dm_img_str,
+    dm_cover_img_str: dm_cover_img_str
+  };
+}
