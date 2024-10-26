288e83210e12e75d5c240843c212b6fac3f58b62
snowtafir <fengxuehuayue@gmail.com>
2024-10-26 23:51:37 +0800

    fix: getBiliUserInfoByUid

--- models/bilibili/bilibili.risk.w_webid.ts
+++ models/bilibili/bilibili.risk.w_webid.ts
@@ -9,7 +9,7 @@ export async function getWebId(uid?: number) {
   const w_webid_key = 'Yz:yuki:bili:w_webid';
   const w_webid = await redis.get(w_webid_key);
   if (w_webid) {
-    return w_webid;
+    return String(w_webid);
   } else {
     const url = `https://space.bilibili.com/${uid ? uid : 401742377}/dynamic`;
     let { cookie } = await readSyncCookie();
@@ -32,7 +32,7 @@ export async function getWebId(uid?: number) {
       const access_id = decoded__RENDER_DATA__JsonString.match(accessIdRegex);
       if (access_id && access_id[1]) {
         await redis.set(w_webid_key, access_id[1], { EX: 43197 * 1000 });
-        return access_id[1];
+        return String(access_id[1]);
       } else {
         console.error('Failed to get access_id from __RENDER_DATA__');
         return null;
