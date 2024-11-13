44dd44d8daadb9bea40ca3b9b171fe57133ef1c5
snowtafir <fengxuehuayue@gmail.com>
2024-11-13 19:01:44 +0800

    optimize: QrCode strict string

--- models/bilibili/bilibili.main.query.ts
+++ models/bilibili/bilibili.main.query.ts
@@ -34,7 +34,7 @@ export class BiliQuery {
         desc = data?.modules?.module_dynamic?.major?.archive || {};
         formatData.data.title = desc?.title;
         formatData.data.content = this.parseRichTextNodes(desc?.desc);
-        formatData.data.url = this.formatUrl(desc?.jump_url);
+        formatData.data.url = this.formatUrl(desc?.jump_url) || '';
         formatData.data.pubTime = author.pub_time;
         formatData.data.pubTs = moment(author.pub_ts * 1000).format('YYYY年MM月DD日 HH:mm:ss');
         formatData.data.category = '视频动态';
@@ -148,7 +148,7 @@ export class BiliQuery {
           formatData.data.pics = pics;
           formatData.data.content = '';
         }
-        formatData.data.url = this.formatUrl(desc?.jump_url);
+        formatData.data.url = this.formatUrl(desc?.jump_url) || '';
         formatData.data.pubTime = author.pub_time;
         formatData.data.pubTs = moment(author.pub_ts * 1000).format('YYYY年MM月DD日 HH:mm:ss');
         formatData.data.category = '文章动态';
