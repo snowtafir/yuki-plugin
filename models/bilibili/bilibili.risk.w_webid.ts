b6cf7ec7aafb6d7c701d9fc611a7114760310401
snowtafir <fengxuehuayue@gmail.com>
2025-01-11 17:51:51 +0800

    fix: w_webid TTL

--- models/bilibili/bilibili.risk.w_webid.ts
+++ models/bilibili/bilibili.risk.w_webid.ts
@@ -8,7 +8,8 @@ declare const redis: any;
 export async function getWebId(uid?: number) {
   const w_webid_key = 'Yz:yuki:bili:w_webid';
   const w_webid = await redis.get(w_webid_key);
-  if (w_webid) {
+  const keyTTL = await redis.ttl(w_webid_key);
+  if (w_webid && keyTTL < 259200) {
     return String(w_webid);
   } else {
     const url = `https://space.bilibili.com/${uid ? uid : 401742377}/dynamic`;
@@ -30,8 +31,11 @@ export async function getWebId(uid?: number) {
       const decoded__RENDER_DATA__JsonString = decodeURIComponent(__RENDER_DATA__[1]);
       const accessIdRegex = /"access_id":"(.*?)"/;
       const access_id = decoded__RENDER_DATA__JsonString.match(accessIdRegex);
-      if (access_id && access_id[1]) {
-        await redis.set(w_webid_key, access_id[1], { EX: 43197 * 1000 });
+      const ExpirationTimeRegex =
+        /document.getElementById\("__RENDER_DATA__"\).*?setTimeout\(function\(\)\s*{window.location.reload\(true\);},\s*(\d+)\s*\*\s*(\d+)\);<\/script>/;
+      const ExpirationTime = htmlContent.match(ExpirationTimeRegex);
+      if (access_id && access_id[1] && ExpirationTime && ExpirationTime[1]) {
+        await redis.set(w_webid_key, access_id[1], { EX: Number(ExpirationTime[1]) });
         return String(access_id[1]);
       } else {
         console.error('Failed to get access_id from __RENDER_DATA__');
